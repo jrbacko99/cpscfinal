058708960f3992bc3f2aa75f6d026e8a74d0ed00
jrbacko99 <jrbacko99@github.com>
2022-12-03 23:54:15 -0500

    db additem troubshoot2

--- included/additem.included.php
+++ included/additem.included.php
@@ -1,12 +1,11 @@
 <?php
 
-require_once 'db.included.php';
-
 if (isset($_POST["submit"])) {
   $itemid = $_POST["itemid"];
   $itemn = $_POST["itemn"];
   $itemq = $_POST["itemq"];
 
+  require_once 'db.included.php';
   // inserts data into inventory table
   $sql = ("INSERT INTO inventory (itemid, itemn, itemq) VALUES ('$itemid', '$itemn', '$itemq')");
 }
