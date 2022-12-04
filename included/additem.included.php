278ee8e7d6fa587f399044fb9487be4be6be1d05
jrbacko99 <jrbacko99@github.com>
2022-12-04 11:47:11 -0500

    old code test

--- included/additem.included.php
+++ included/additem.included.php
@@ -1,39 +1,31 @@
 <?php
-
 $servername = "172.16.99.2";
 $username = "jbacko";
 $password = "student";
 $dbname = "warehouse";
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if(!$conn) {
-  die("Connection failed");
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
 
-if (isset($_POST["submit"])) {
+$itemid = $_POST["itemid"];
+$itemn = $_POST["itemn"];
+$itemq = $_POST["itemq"];
 
-  $itemid = $_POST["itemid"];
-  $itemn = $_POST["itemn"];
-  $itemq = $_POST["itemq"];
-
-  if (empty($itemid) || empty($itemn) || empty($itemq)) {
-    echo 'Please fill in all of the information';
-  }
-  else{
-    // inserts data into inventory table
-    $query = ("INSERT INTO inventory (itemid, itemn, itemq) VALUES ('$itemid', '$itemn', '$itemq')");
-    $result = mysqli_query($conn,$query);
-
-    if ($result) {
-      header("location: ../additem.php");
-      echo "You have successfully added your item";
+$sql = ("INSERT INTO inventory (itemid, itemn, itemq) VALUES ('$itemid', '$itemn', '$itemq')");
+//insert to php database
+if ($conn->query($sql) === TRUE)
+    {
+    echo "You have successfully added your item";
     }
-    else {
-      echo "Your record has NOT been added. Please check your input.";
+
+else
+    {
+    echo "Error: " . $sql . "<br>" . $conn->error;
     }
-  }
-}
+$conn->close();
+
 ?>
