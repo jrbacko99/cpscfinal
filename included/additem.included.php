56018230a05d36723f115a60a194d39ab7fa2afd
jrbacko99 <jrbacko99@github.com>
2022-12-03 23:27:19 -0500

    db additem troubshoot

--- included/additem.included.php
+++ included/additem.included.php
@@ -10,9 +10,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-
 if (isset($_POST["submit"])) {
-
   $itemid = $_POST["itemid"];
   $itemn = $_POST["itemn"];
   $itemq = $_POST["itemq"];
