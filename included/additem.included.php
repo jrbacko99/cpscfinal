f7e1a1c27d13e135958fb164fff5de5c540df567
jrbacko99 <jrbacko99@github.com>
2022-12-04 00:06:05 -0500

    db additem troubshoot2

--- included/additem.included.php
+++ included/additem.included.php
@@ -1,16 +1,30 @@
 <?php
+$servername = "172.16.99.2";
+$username = "jbacko";
+$password = "student";
+$dbname = "warehouse";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
 if (isset($_POST["submit"])) {
   $itemid = $_POST["itemid"];
   $itemn = $_POST["itemn"];
   $itemq = $_POST["itemq"];
 
-  require_once 'db.included.php';
   // inserts data into inventory table
   $sql = ("INSERT INTO inventory (itemid, itemn, itemq) VALUES ('$itemid', '$itemn', '$itemq')");
+
+  if ($conn->query($sql) === TRUE) {
+    echo "You have successfully added your item";
+    header("location: ../additem.php");
+  }
+  else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
 }
-else {
-  header("location: ../profile.php");
-  exit();
-}
+$conn->close();
 ?>
