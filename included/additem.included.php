af6d925c083aa91e43c228dee80325d6df01126e
jrbacko99 <jrbacko99@github.com>
2022-12-04 09:56:10 -0500

    testing connection additem

--- included/additem.included.php
+++ included/additem.included.php
@@ -1,36 +1,37 @@
 <?php
-$servername = "172.16.99.2";
-$username = "jbacko";
-$password = "student";
-$dbname = "warehouse";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-  echo "connection worked";
-}
 if (isset($_POST["submit"])) {
 
   $itemid = $_POST["itemid"];
   $itemn = $_POST["itemn"];
   $itemq = $_POST["itemq"];
-  echo "submit button worked";
 
-    // inserts data into inventory table
-  $sql = ("INSERT INTO inventory (itemid, itemn, itemq) VALUES ('$itemid', '$itemn', '$itemq')");
-    echo "sql statement worked";
-  if ($conn->query($sql) === TRUE) {
-    echo "You have successfully added your item";
-    header("location: ../additem.php");
+  $servername = "172.16.99.2";
+  $username = "jbacko";
+  $password = "student";
+  $dbname = "warehouse";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  echo "submit button worked and connect created";
+  // Check connection
+//  if($conn->connect_error) {
+    //die("Connection failed: " . $conn->connect_error);
+    //echo "connection worked";
+  //}
+
+      // inserts data into inventory table
+    //$sql = ("INSERT INTO inventory (itemid, itemn, itemq) VALUES ('$itemid', '$itemn', '$itemq')");
+      //echo "sql statement worked";
+    //if ($conn->query($sql) === TRUE) {
+      //echo "You have successfully added your item";
+      //header("location: ../additem.php");
+    //}
+    //else {
+      //echo "Error: " . $sql . "<br>" . $conn->error;
+      //echo "Your record has NOT been added";
+    //}
+  //$conn->close();
+//  header("location: ../profile.php");
+//  exit();
   }
-  else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-    echo "Your record has NOT been added";
-  }
-$conn->close();
-header("location: ../profile.php");
-exit();
-}
 ?>
