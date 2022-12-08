8cc89f88dc26e33f3ab8c3b865dc54101dc550da
jrbacko99 <jrbacko99@github.com>
2022-12-08 15:17:24 -0500

    footer change

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <footer>
+<b>Completed by CyberPunkz 12/8/2022</b><br><br>
 Copyright &copy; CyberPunkz CPSC-336 Project<br>
 <a href="mailto:CyberPunkz@CPSC-336.com">CyberPunkz@CPSC-336.com</a>
 </footer>
